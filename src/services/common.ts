a905804c913a565e805a5d5a32af02d9b28c676f
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2022-12-28 20:36:55 +0100

    feat: use a custom baseQuery that wraps around fetchBaseQuery to force logout when an api returns statuscode 401 or 403. See issue https://github.com/reduxjs/redux-toolkit/issues/3040. Close issue #5
    Signed-off-by: Stefano Cappa <stefano.cappa.ks89@gmail.com>

--- src/services/common.ts
+++ src/services/common.ts
@@ -1,24 +1,39 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
-import { getToken } from '../auth/auth-utils';
+import { getToken, removeToken } from '../auth/auth-utils';
+import { BaseQueryApi, BaseQueryFn, FetchArgs, FetchBaseQueryError } from '@reduxjs/toolkit/query';
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: '/api',
+  prepareHeaders: (headers) => {
+    headers.set('Content-Type', 'application/json');
+    const token: string | null = getToken('token');
+    if (token) {
+      headers.set('Authorization', 'Bearer ' + token);
+    }
+    return headers;
+  },
+});
+
+const baseQueryWithForceLogout: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> =
+  async (args: string | FetchArgs, api: BaseQueryApi, extraOptions) => {
+    const result = await baseQuery(args, api, extraOptions);
+    console.log('baseQueryWithForceLogout - result=', result);
+    if (result.error && (result.error.status === 401 || result.error.status === 403)) {
+      console.log('baseQueryWithForceLogout - status 401 or 403 - logging out');
+      //  1. Redirect user to LOGIN
+      //  2. Reset authentication from localstorage/sessionstorage
+      removeToken();
+      window.location.href = '/';
+    }
+    return result;
+  };
 
 export const commonApi = createApi({
   reducerPath: 'api',
   // keepUnusedDataFor: specifies how long the data should be kept in the cache
   // after the subscriber reference count reaches zero.
   keepUnusedDataFor: 60, // invalidate cache after 60 seconds
-  baseQuery: fetchBaseQuery({
-    baseUrl: '/api/',
-    prepareHeaders: headers => {
-      headers.set('Content-Type', 'application/json');
-      const token: string | null = getToken('token');
-      if (token) {
-        headers.set('Authorization', 'Bearer ' + token);
-      }
-      return headers;
-    },
-    // validateStatus:(response, body) => {},
-    // fetchFn:
-  }),
+  baseQuery: baseQueryWithForceLogout,
   tagTypes: ['Login', 'Homes', 'Rooms', 'Devices', 'SensorValues', 'ACValue', 'Profile'],
   endpoints: _ => ({}),
 });
