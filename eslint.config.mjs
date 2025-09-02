f2b4ab8f462ea74e974e6eead12a0bac211446dc
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2025-09-02 21:03:16 +0200

    feat: upgrade to the latest version of nx
    Signed-off-by: Stefano Cappa <stefano.cappa.ks89@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-const nx = require('@nx/eslint-plugin');
+import nx from '@nx/eslint-plugin';
 
-module.exports = [
+export default [
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
@@ -15,6 +15,8 @@ module.exports = [
     files: [
       '**/*.ts',
       '**/*.tsx',
+      '**/*.cts',
+      '**/*.mts',
       '**/*.js',
       '**/*.jsx',
       '**/*.cjs',
--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,27 @@
-/// <reference types="vitest" />
+/// <reference types='vitest' />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
 export default defineConfig({
   root: __dirname,
-  build: {
-    outDir: 'dist/home-anthill',
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
   cacheDir: './node_modules/.vite/home-anthill',
-
   server: {
     port: 4200,
     host: 'localhost',
   },
-
   preview: {
     port: 4300,
     host: 'localhost',
   },
-
-  plugins: [react(), nxViteTsPaths()],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
-  test: {
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: 'coverage/home-anthill',
-      provider: 'v8',
+  plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+  build: {
+    outDir: 'dist/home-anthill',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
     },
-    globals: true,
-    cache: {
-      dir: './node_modules/.vitest',
-    },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
 });
